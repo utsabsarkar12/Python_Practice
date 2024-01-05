0ee7a580fb8d5ea955dd1d017999481b8ccec03d
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2024-01-05 23:10:40 +0600

    type conversion

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,2 +1,13 @@
 # show the output of a program
 print("Hello World")
+
+# variables
+name = "utsab"
+age = 22
+salary = 40000.00
+print(name, age)
+print(salary)
+
+# input from user
+name = input("What is your name?\n")
+print("Name:" +name)
