84a9bc634ce130837318ad49324e3f313224c8e5
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2024-01-06 20:45:05 +0600

    string practice in python

---
+++ String.py
@@ -0,0 +1,17 @@
+name = "Utsab Sarkar"
+
+print(name)
+print(name.upper())
+print(name)
+print(name.lower())
+print(name)
+print(name.find('S'))
+print(name.find('e'))
+print(name.find('sab'))
+print(name.replace("Utsab Sarkar", "Shuvo"))
+print(name)
+print(name.replace("Utsab", "Shuvo"))
+print(name)
+
+print('u' in name)
+print('S' in name)
---
+++ TypeConversion.py
@@ -0,0 +1,11 @@
+# type conversion in int
+old_age = input("Enter your age: ")
+new_age = int(old_age) + 2
+print(new_age)
+
+# type conversion in float
+first_number = input("Enter First Number: ")
+second_number = input("Enter Second Number: ")
+
+sum = int(first_number) + int(second_number)
+print("The sum is " +str(sum))
