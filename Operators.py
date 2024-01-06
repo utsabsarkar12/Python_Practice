841de36795a39704cdb1baad830d693f26b898dd
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2024-01-06 23:24:56 +0600

    operator practice in python

---
+++ Operators.py
@@ -0,0 +1,14 @@
+# arithmetic operators
+num1 = 10
+num2 = 2
+sum = num1 + num2
+print(float(sum))
+
+sub = num1 - num2
+print(sub)
+
+mul = num1 * num2
+print(mul)
+
+div = num1 / num2
+print(int(div))
