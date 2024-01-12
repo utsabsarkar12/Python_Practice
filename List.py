138330c0b5424c6bf23c729e4f85d41832c30230
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2024-01-12 22:40:34 +0600

    practice list

---
+++ List.py
@@ -0,0 +1,17 @@
+# python list
+marks = [98, 95, 97, 100, 89]
+print(marks)
+print(marks[1])
+print(marks[-1])
+print(marks[0:3])
+
+# for loop in list
+for score in marks:
+    print(score)
+
+marks.append(99)
+print(marks)
+marks.insert(0 , 99)
+print(marks)
+print(93 in marks)
+print(len(marks))
