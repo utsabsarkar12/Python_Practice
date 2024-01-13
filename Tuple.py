48d4ab1b770847a3369368886fd29c61116a9066
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2024-01-13 22:09:16 +0600

    tuple practice

---
+++ Tuple.py
@@ -0,0 +1,7 @@
+# tuple is one kind of list. here we use () in tuple replace of [] in list. tuple is unchangeable.
+
+marks = (99, 90, 97, 95, 97)
+
+print(marks)
+print(marks.count(97))
+print(marks.index(95))
