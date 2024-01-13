cbc2e990b29bd02307f478bd6c49b59aecc00ac5
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2024-01-13 22:31:15 +0600

    practice set

---
+++ Set.py
@@ -0,0 +1,9 @@
+# set is a kind of list. where we use {} in set replace of () in list
+# set doesn't count same element in two times
+
+marks = {99, 90, 97, 95, 97}
+
+print(marks)
+
+for score in marks:
+    print(score)
