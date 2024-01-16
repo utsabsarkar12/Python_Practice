b3778cba9ebb41ca70c496fb6b5aacd44add19b0
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2024-01-16 20:36:47 +0600

    multiple variables practice

--- FirstProgram.py
+++ FirstProgram.py
@@ -8,6 +8,12 @@ salary = 40000.00
 print(name, age)
 print(salary)
 
+# multiple variables
+x, y, z = 10, 100, 100
+print(x)
+print(y)
+print(z)
+
 # input from user
 name = input("What is your name?\n")
 print("Name:" +name)
