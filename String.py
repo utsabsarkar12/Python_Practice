9efdfbbe8d5df1f7127468a4770cfcdb947cf422
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2024-01-20 21:19:23 +0600

    string practice

--- String.py
+++ String.py
@@ -5,6 +5,7 @@ print(name.upper())
 print(name)
 print(name.lower())
 print(name)
+
 print(name.find('S'))
 print(name.find('e'))
 print(name.find('sab'))
@@ -15,3 +16,15 @@ print(name)
 
 print('u' in name)
 print('S' in name)
+
+print(name.index('b'))
+
+new_name = "shUvo sArkar"
+print(new_name.capitalize())
+print(new_name.title())
+
+x = 122
+y = 124
+z = x * y
+
+print("I have {} taka".format(z))
