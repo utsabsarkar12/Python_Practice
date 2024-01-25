159804d3978cd414af0625b4b300725207a58f2d
LAPTOP-32363BRT\ACER <utsab.heart@gmail.com>
2024-01-25 23:21:00 +0600

    class & object practice

---
+++ OOP/Class&Object.py
@@ -0,0 +1,17 @@
+class Human:
+    name = ""
+    age = ""
+
+human1 = Human()
+
+human1.name = "Utsab"
+human1.age = 22
+print(human1.name)
+print(human1.age)
+
+human2 = Human()
+
+human2.name = "Shuvo"
+human2.age = 32
+print(human2.name)
+print(human2.age)
